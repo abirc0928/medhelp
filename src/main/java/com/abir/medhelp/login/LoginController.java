a0d21c19be838033f790ac7f7b5183765dfc8ae8
abirc0928 <abirchowdhury0928@gmail.com>
2021-09-13 13:09:04 +0600

    simple controller

---
+++ src/main/java/com/abir/medhelp/login/LoginController.java
@@ -0,0 +1,13 @@
+package com.abir.medhelp.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+	@GetMapping("/hello")
+    public String getTreeById() {
+        return "Hello world";
+    }
+}
