4ac1928710a0169508057ca48fe33bd7ea2af29b
abirc0928 <abirchowdhury0928@gmail.com>
2021-09-13 12:12:28 +0600

    initial commit

---
+++ src/main/java/com/abir/medhelp/MedhelpApplication.java
@@ -0,0 +1,13 @@
+package com.abir.medhelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedhelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedhelpApplication.class, args);
+	}
+
+}
