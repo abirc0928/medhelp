06b46b6a88169f8a604b126879d8bb9da8459c9c
Rabby Chowdhury <you@example.com>
2021-09-14 18:08:20 +0600

    custome jpa query

--- src/main/java/com/abir/medhelp/user/UserRepository.java
+++ src/main/java/com/abir/medhelp/user/UserRepository.java
@@ -1,5 +1,6 @@
 package com.abir.medhelp.user;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,6 @@ import com.abir.medhelp.entity.UserEntity;
 @Repository
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
 
+	@Query(value = "SELECT * FROM USER WHERE user_mail = ?1", nativeQuery = true)
 	public UserEntity findByUserMail(String userMail);
 }
